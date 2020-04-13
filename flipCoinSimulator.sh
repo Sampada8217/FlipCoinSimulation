baa19bf240f5e1f6eb46d9d6bcac0c21db7cb035
sampada8217 <sampadatirthkar@gmail.com>
2020-04-14 00:38:38 +0530

    Added Code for Triplet Combination and Determine Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -60,3 +60,65 @@ percentHH=$(( ($HH * 100 ) / $flip1 ))
 percentHT=$(( ($HT * 100 ) / $flip1 ))
 percentTH=$(( ($TH * 100 ) / $flip1 ))
 percentTT=$(( ($TT * 100 ) / $flip1 ))
+
+
+#Code for Triplet Combination
+echo "Enter no of flip"
+read flip2
+HHH=0
+HTH=0
+HTT=0
+HHT=0
+TTT=0
+THT=0
+TTH=0
+THH=0
+count1=0
+while [ $count1 -ne $flip2 ]
+do
+	random=$((RANDOM%2))
+        random1=$((RANDOM%2))
+	random2=$((RANDOM%2))
+	if [ $random -eq  1 ] && [ $random1 -eq 1 ] && [ $random2 -eq 1 ]
+        then
+		echo "HHH"
+ 		((HHH++))
+	elif [ $random -eq 1 ] && [ $random1 -eq 0 ] && [ $random2 -eq 1 ]
+	then
+		echo "HTH"
+		((HTH++))
+	elif [ $random -eq 1 ] && [ $random1 -eq 0 ] && [ $random2 -eq 0 ]
+	then
+		echo "HTT"
+		((HTT++))
+	elif [ $random -eq 1 ] && [ $random1 -eq 1 ] && [ $random2 -eq 0 ]
+	then
+		echo "HHT"
+		((HHT++))
+	elif [ $random -eq 0 ] && [ $random1 -eq 0 ] && [ $random2 -eq 0 ]
+	then
+		echo "TTT"
+		((TTT++))
+	elif [ $random -eq 0 ] && [ $random1 -eq 1 ]  && [ $random2 -eq 1 ]
+	then
+		echo "THH"
+		((THH++))
+	elif [ $random -eq 0 ] && [ $random1 -eq 1 ] && [ $random2 -eq  0 ]
+	then
+		echo "THT"
+		((THT++))
+	else
+      		echo "TTH"
+		((TTH++))
+	fi
+	((count1++))
+done
+percentHHH=$(( ( $HHH * 100 ) / $flip2 ))
+percentHTH=$(( ( $HTH * 100 ) / $flip2 ))
+percentHTT=$(( ( $HTT * 100 ) / $flip2 ))
+percentHHT=$(( ( $HHT * 100 ) / $flip2 ))
+percentTTT=$(( ( $TTT * 100 ) / $flip2 ))
+percentTHT=$(( ( $THT * 100 ) / $flip2 ))
+percentTTH=$(( ( $TTH * 100 ) / $flip2 ))
+percentTHH=$(( ( $THH * 100 ) / $flip2 ))
+
