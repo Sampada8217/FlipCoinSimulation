be6373e1db0f2a6336d8c94baef0fb1627ce53d4
sampada8217 <sampadatirthkar@gmail.com>
2020-04-13 21:57:41 +0530

    Added Code to Store Singlet Combination in Dictionary and Determine Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
-
+count=0
+head=0
+tail=0
+while [ $count -ne 6 ]
+do  
+        counter=0
+        declare -A singlet
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+	       echo "Head"
+               singlet[counter++]="Head"
+               ((head++))
+	else
+		echo "Tail"
+              singlet[counter++]="Tail"
+   	       ((tail++))
+	fi
+      ((count++))
+done
+singlet[0]=$head
+singlet[1]=$tail
+percentHead=$(( ( $head * 100 ) / 6 ))
+percentTail=$(( ($tail * 100 ) / 6 ))
+   
