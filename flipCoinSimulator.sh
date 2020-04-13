45e8196eb0970ad642cc13d0da7aa69db7e96383
sampada8217 <sampadatirthkar@gmail.com>
2020-04-13 22:59:21 +0530

    Added Code of Storing Doublet Combination and Determine Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -24,3 +24,39 @@ singlet[1]=$tail
 percentHead=$(( ( $head * 100 ) / 6 ))
 percentTail=$(( ($tail * 100 ) / 6 ))
    
+
+#Code For Doublet Combination
+
+HH=0
+HT=0
+TH=0
+TT=0
+echo "Enter no of flips"
+read flip1
+count=0
+while [ $count -ne $flip1 ]
+do
+	random=$((RANDOM%2))
+        random1=$((RANDOM%2))
+        if [ $random -eq 1 ] && [ $random1 -eq  1 ] 
+        then
+             echo "HH"
+              ((HH++))
+        elif [  $random -eq 1 ] && [ $random1 -eq 0 ] 
+        then
+            echo "HT"
+              ((HT++))
+        elif [  $random -eq 0 ] && [ $random1 -eq 1  ] 
+        then
+             echo "TH"
+              ((TH++))
+        else
+             echo "TT"
+             ((TT++))
+        fi
+        ((count++))
+done
+percentHH=$(( ($HH * 100 ) / $flip1 ))
+percentHT=$(( ($HT * 100 ) / $flip1 ))
+percentTH=$(( ($TH * 100 ) / $flip1 ))
+percentTT=$(( ($TT * 100 ) / $flip1 ))
